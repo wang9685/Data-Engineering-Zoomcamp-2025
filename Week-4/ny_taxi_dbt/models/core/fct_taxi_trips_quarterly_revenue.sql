a881b5c1cb8eda30fd2978bd22ee86a2751a37fe
I-Shu <ishuwang9685@gmail.com>
2025-02-26 04:41:11 +0000

    sql

--- Week-4/ny_taxi_dbt/models/core/fct_taxi_trips_quarterly_revenue.sql
+++ Week-4/ny_taxi_dbt/models/core/fct_taxi_trips_quarterly_revenue.sql
@@ -6,30 +6,24 @@
 
 WITH quarterly_revenue AS (
     SELECT
-        taxi_type,  -- 'green' or 'yellow'
+        service_type,  -- 'green' or 'yellow'
         EXTRACT(YEAR FROM pickup_datetime) AS year,
         EXTRACT(QUARTER FROM pickup_datetime) AS quarter,
         SUM(total_amount) AS total_revenue
     FROM
-        `your_project.your_dataset.fact_taxi_trips`
+        `zoomcamp_week4.fact_trips` 
     GROUP BY
-        taxi_type, year, quarter
-),
+        service_type, year, quarter
+)
 
+SELECT 
+    year,
+    quarter,
+    total_revenue,
+    service_type,
+    total_revenue - LAG(total_revenue) OVER (PARTITION BY service_type, quarter ORDER BY year) AS revenue_diff
+FROM quarterly_revenue
+ORDER BY revenue_diff
 
-SELECT
-    a.taxi_type,
-    a.year,
-    a.quarter,
-    a.total_revenue,
-    b.total_revenue AS previous_year_revenue,
-    SAFE_DIVIDE((a.total_revenue - b.total_revenue), b.total_revenue) * 100 AS yoy_growth_percentage
-FROM
-    quarterly_revenue a
-LEFT JOIN
-    quarterly_revenue b
-    ON a.taxi_type = b.taxi_type
-    AND a.quarter = b.quarter
-    AND a.year = b.year + 1
 
 
